6fd069855d706e0236596999dc60dc8781e3ee49
Xilong Yang <contact@xilong.site>
2024-05-30 10:59:59 +0800

    2.74

--- chapter2/2.74/tsub_ok.c
+++ chapter2/2.74/tsub_ok.c
@@ -25,35 +25,30 @@
 #include <stdio.h>
 #include <limits.h>
 
-int saturating_add(int x, int y) {
+int tsub_ok(int x, int y) {
     int w = sizeof(int) << 3;
     int sign_x = (unsigned) x >> (w - 1);
     int sign_y = (unsigned) y >> (w - 1);
 
-    int result = x + y;
+    int result = x - y;
     int sign_result = (unsigned) result >> (w - 1);
 
-    // Result is overflow only if the sign of x equals the sign of y, and the sign of x (same as y) not equals the sign of result.
-    // Figure out 1 when it's overflow, and 0 otherwise.
-    int overflow_flag = !sign_x^sign_y && sign_x^sign_result;
+    // Result is overflow only if the sign of x not equals the sign of y, and the sign of x not equals the sign of result.
+    // Yield 1 when it's overflow, and 0 otherwise.
+    int overflow_flag = sign_x^sign_y && sign_x^sign_result;
 
-    overflow_flag && sign_result && (result = INT_MIN);
-    overflow_flag && !sign_result && (result = INT_MAX);
-
-    return result;
+    return !overflow_flag;
 }
 
 int main() {
-    printf("%d\n", saturating_add(0, 0));
-    printf("%d\n", saturating_add(0, 1));
-    printf("%d\n", saturating_add(1, 0));
-    printf("%d\n", saturating_add(1, 1));
-    printf("%d\n", saturating_add(1, -1));
-    printf("%d\n", saturating_add(INT_MAX, INT_MIN));
-    printf("%d\n", saturating_add(INT_MAX, 1));
-    printf("%d\n", saturating_add(INT_MIN, -1));
-    printf("%d\n", saturating_add(INT_MAX, INT_MAX));
-    printf("%d\n", saturating_add(INT_MIN, INT_MIN));
+    printf("%d\n", tsub_ok(0, 0));
+    printf("%d\n", tsub_ok(0, 1));
+    printf("%d\n", tsub_ok(1, 0));
+    printf("%d\n", tsub_ok(1, 1));
+    printf("%d\n", tsub_ok(INT_MIN, 1));
+    printf("%d\n", tsub_ok(INT_MAX, -1));
+    printf("%d\n", tsub_ok(INT_MIN, INT_MAX));
+    printf("%d\n", tsub_ok(INT_MAX, INT_MIN));
     return 0;
 }
 
