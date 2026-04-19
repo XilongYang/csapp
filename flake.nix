cea2367d630e1dd576e6d8d771158757f9f941d2
Xilong Yang <contact@xilong.site>
2026-04-19 16:11:03 +0900

    flake.nix

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        gcc
+        gdb
+      ];
+    };
+  };
+}
