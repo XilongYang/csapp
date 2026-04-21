2b7b6c8c076c3fc33fe2204c51bad1dd5756c01b
Xilong Yang <contact@xilong.site>
2026-04-21 20:57:28 +0900

    3.62

---
+++ chapters/3/62/switch3.asm
@@ -0,0 +1,26 @@
+// p1 in %rdi, p2 in %rsi, action in %edx
+.L8: // MODE_E
+  movl  $27, %eax
+  ret
+.L3: // MODE_A
+  movq  (%rsi), %rax
+  movq  (%rdi), %rdx
+  movq  %rdx, (%rsi)
+  ret
+.L5: // MODE_B
+  movq  (%rdi), %rax
+  addq  (%rsi), %rax
+  movq  %rax, (%rdi)
+  ret
+.L6: // MODE_C
+  movq  $59, (%rdi)
+  movq  (%rsi), %rax
+  ret
+.L7: // MODE_D
+  movq  (%rsi), %rax
+  movq  %rax, (%rdi)
+  movl  $27, %eax
+  ret
+.L9: // default
+  movl  $12, %eax
+  ret
